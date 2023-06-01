5bd2e6e4c9c88b3d8aa0485f3fe1e6e921d37d4a
Eli Dawson <edawson@alumni.berklee.edu>
2023-06-01 00:07:35 -0400

    Completed

--- lib/classes/national_park.py
+++ lib/classes/national_park.py
@@ -5,16 +5,32 @@ class NationalPark:
         self._trips = []
         self._visitors = []
         
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and not hasattr(self, 'name'):
+            self._name = name 
+        else:
+            raise Exception
+        
     def trips(self, new_trip=None):
         from classes.trip import Trip
-        pass
+        if new_trip and isinstance(new_trip, Trip):
+            self._trips.append(new_trip)
+        return self._trips
     
     def visitors(self, new_visitor=None):
         from classes.visitor import Visitor
-        pass
+        if new_visitor and isinstance(new_visitor, Visitor):
+            self._visitors.append(new_visitor)
+        return list(set(self._visitors))
     
     def total_visits(self):
-        pass
+        return len(self._trips)
     
     def best_visitor(self):
-        pass
+        return max(set(self._visitors), key=self._visitors.count)
